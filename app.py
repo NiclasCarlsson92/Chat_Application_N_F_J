9459cc7d1fc513362e6584c7a8ff2df5db901b80
Niclas Carlsson <niclas.carlsson@student.kyh.se>
2022-01-26 10:07:52 +0100

    Changes made to models and db

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import dotenv
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
--- models.py
+++ models.py
@@ -3,9 +3,11 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(100))
     admin = db.Column(db.BOOLEAN, default=False)
+    online = db.Column(db.BOOLEAN, default=False)
 
     def is_active(self):
         return True
