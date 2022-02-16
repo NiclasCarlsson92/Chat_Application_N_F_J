247b858460dfb5f639f49a7b88061ac2a67e5469
Niclas Carlsson <niclas.carlsson@student.kyh.se>
2022-02-16 19:15:56 +0100

    Crypto Chat Workinggit add . Encryption remains..

--- Chat_VG/chat_act_server.py
+++ Chat_VG/chat_act_server.py
@@ -1,21 +1,22 @@
 import socket
 import threading
 import queue
-from chat_act_client import CLIENT_USERNAME
 
 hostname = socket.gethostname()
 HOST = socket.gethostbyname(hostname)
-PORT = 10007
+PORT = 9876
 connected_clients = []
-USERNAME = "HOST"
+server_username = "Bob"
+client_username = ""
 
 
 def client_handler(client_socket, broadcast_queue, client_list):
+    from chat_act_client import client_username
     while True:
         try:
             message = client_socket.recv(1024)
             message = message.decode('utf-8')
-            print('CLIENT > ', message)
+            print(f'{client_username}: {message}')
 
             message_dict = {
                 'sender_socket': client_socket,
@@ -37,27 +38,27 @@ def broadcast(client_list, broadcast_queue):
 
 
 def main():
+    global server_username, client_username
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f'Chat Server hosted at: {HOST}:{PORT}')
+    print('Waiting for connections...')
     server_socket.bind((HOST, PORT))
-    server_socket.listen(5)
+    server_socket.listen()
+    client_socket, client_address = server_socket.accept()
     client_list = []
     broadcast_queue = queue.Queue()
-
-    broadcast_thread = threading.Thread(target=broadcast, args=(client_list, broadcast_queue))
-    broadcast_thread.start()
-    print('Welcome', USERNAME)
-    print(f'Chat Server @{HOST}')
-    print('Waiting for connections...')
+    print(f'Got a connection from {client_address}')
 
     while True:
-        client_socket, client_address = server_socket.accept()
-        print(f'Got a connection from {client_address}')
+        broadcast_thread = threading.Thread(target=broadcast, args=(client_list, broadcast_queue))
+        broadcast_thread.start()
+        client_message = input('> ')
+        client_message = client_message.encode('utf-8')
+        client_socket.send(client_message)
         client_thread = threading.Thread(target=client_handler,
                                          args=(client_socket, broadcast_queue, client_list))
         client_list.append(client_socket)
         client_thread.start()
-        client_thread.join()
-
 
 
 if __name__ == '__main__':
