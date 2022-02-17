7539ab5a571a9313617dde95dd0b726a59a813c6
Niclas Carlsson <niclas.carlsson@student.kyh.se>
2022-02-17 14:22:41 +0100

    Crypto Chat running w Encryption

--- Chat_VG/aes_rsa.py
+++ Chat_VG/aes_rsa.py
@@ -1,7 +1,4 @@
-import os
-from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
 
 
 def gen_keys(key_name, passphrase, key_size):
@@ -14,14 +11,6 @@ def gen_keys(key_name, passphrase, key_size):
 
     with open(f'./rsa_keys_chat/{key_name}_priv.key', 'wb') as priv_file:
         priv_file.write(private_key)
-
-
-def read_public_key(key_name):
-    return RSA.importKey(open(f'./rsa_keys_chat/{key_name}_pub.key', 'r').read())
-
-
-def read_private_key(key_name, passphrase):
-    return RSA.importKey(open(f'./rsa_keys_chat/{key_name}_priv.key', 'r').read(), passphrase)
 
 
 def generate_keys():
@@ -44,75 +33,16 @@ def generate_keys():
     gen_keys(key_name, passphrase, key_size)
 
 
-def encrypt_message():
-    message = input('Enter message to encrypt: ').encode('utf-8')
-    while True:
-        key_name = input('Enter name of recipient: ')
-        if not os.path.exists(f'./rsa_keys_chat/{key_name}_pub.key'):
-            print(f'Can\'t find key file named {key_name}_pub.key. Please try again.')
-            continue
-        break
-
-    public_key = read_public_key(key_name)
-
-    encrypted_file_name = input('Enter file name for encrypted message: ')
-
-    session_key = get_random_bytes(16)
-
-    rsa_cipher = PKCS1_OAEP.new(public_key)
-    encrypted_session_key = rsa_cipher.encrypt(session_key)
-
-    aes_cipher = AES.new(session_key, AES.MODE_EAX)
-    cipher_text, tag = aes_cipher.encrypt_and_digest(message)
-
-    values = [encrypted_session_key, aes_cipher.nonce, tag, cipher_text]
-    with open(encrypted_file_name + '.enc', 'wb') as out_file:
-        for value in values:
-            out_file.write(value)
-    print(f'The encrypted message was saved in the file {encrypted_file_name}.enc')
-
-
-def decrypt_message():
-    while True:
-        file_name = input('Please enter the name of the encrypted message: ')
-        if not os.path.exists(f'{file_name}.enc'):
-            print(f'Can\'t find file named {file_name}.enc. Please try again.')
-            continue
-        break
-    private_key_name = input('Enter the name of the private key to use: ')
-    passphrase = input('Enter the passphrase used for this key: ')
-
-    private_key = read_private_key(private_key_name, passphrase)
-    with open(f'{file_name}.enc', 'rb') as in_file:
-        enc_session_key, nonce, tag, cipher_text = [in_file.read(b) for b in (private_key.size_in_bytes(), 16, 16, -1)]
-
-    rsa_cipher = PKCS1_OAEP.new(private_key)
-    session_key = rsa_cipher.decrypt(enc_session_key)
-
-    aes_cipher = AES.new(session_key, AES.MODE_EAX, nonce)
-    message = aes_cipher.decrypt_and_verify(cipher_text, tag)
-    print("The decrypted message is:")
-    print(message.decode('utf-8'))
-
 
 def menu():
     while True:
         print('>>>>> ENCRYPTION MENU <<<<<')
         print('===========================')
         print('1. Generate RSA keys')
-        print('2. Encrypt message')
-        print('3. Decrypt message')
-        print()
-        print('9. Exit')
         choice = input('> ')
 
         if choice == '1':
             generate_keys()
-        elif choice == '2':
-            encrypt_message()
-        elif choice == '3':
-            decrypt_message()
-        elif choice == '9':
             break
 
 
--- Chat_VG/chat_act_client.py
+++ Chat_VG/chat_act_client.py
@@ -1,41 +1,77 @@
 import socket
+import sys
 import threading
+import base64
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature import pkcs1_15
 
-LOCALHOST = ""
 HOST = '192.168.0.100'
 PORT = 9876
-client_username = "Alice"
-server_username = ""
+username = "niclas"
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-def send(client_socket):
-    while True:
-        message = input('> ')
-        client_socket.send(message.encode('utf-8'))
-
-
-def recv(client_socket):
-    from chat_act_server import server_username
-    while True:
-        message = client_socket.recv(1024)
-        message = message.decode('utf-8')
-        print(f'{server_username}:{message}')
+def read_private_key(key_name, passphrase):
+    return RSA.importKey(open(f'./rsa_keys_chat/{key_name}_priv.key', 'r').read(), passphrase)
 
 
 def main():
-    global client_username
+    passphrase = input('Enter your passphrase: ')
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((HOST, PORT))
-    print("Chat is connected")
-    from chat_act_server import server_username
-    while True:
-        message = client_socket.recv(1024)
-        message = message.decode('utf-8')
-        input_thread_send = threading.Thread(target=send, args=(client_socket,))
-        input_thread_recv = threading.Thread(target=recv, args=(client_socket,))
-        input_thread_send.start()
-        input_thread_recv.start()
-        print(f'{server_username}:{message}')
+    client_socket.send(username.encode('utf-8'))
+    confirmation_bytes = client_socket.recv(1024)
+
+    private_key = read_private_key(username, passphrase)
+
+    hashed_bytes = SHA256.new(confirmation_bytes)
+    signature = pkcs1_15.new(private_key).sign(hashed_bytes)
+    client_socket.send(signature)
+
+    response = client_socket.recv(1024).decode('utf-8')
+    if response != "Welcome":
+        print(response)
+        sys.exit()
+    encrypted_session_key = client_socket.recv(1024)
+
+    rsa_cipher = PKCS1_OAEP.new(private_key)
+    session_key = rsa_cipher.decrypt(encrypted_session_key)
+    print('Connection is OK, SESSION LIVE!')
+
+    def send():
+        while True:
+            aes_cipher = AES.new(session_key, AES.MODE_EAX)
+            message = input('> ')
+            cipher_text, tag = aes_cipher.encrypt_and_digest(message.encode('utf-8'))
+            nonce = base64.b64encode(aes_cipher.nonce).decode('utf-8')
+            tag = base64.b64encode(tag).decode('utf-8')
+            cipher_text = base64.b64encode(cipher_text).decode('utf-8')
+            encrypted_message = ','.join((nonce, tag, cipher_text))
+            client_socket.send(encrypted_message.encode('utf-8'))
+
+    def recv():
+        from chat_act_server import server_username
+        while True:
+            message = client_socket.recv(1024)
+            nonce, tag, cipher_text = message.decode('utf-8').split(',')
+            nonce = base64.b64decode(nonce)
+            tag = base64.b64decode(tag)
+            cipher_text = base64.b64decode(cipher_text)
+
+            aes_cipher = AES.new(session_key, AES.MODE_EAX, nonce)
+            clear_text = aes_cipher.decrypt_and_verify(cipher_text, tag)
+
+            message = clear_text.decode('utf-8')
+            print(f'{server_username}:{message}')
+
+    input_thread_send = threading.Thread(target=send)
+    input_thread_recv = threading.Thread(target=recv)
+    input_thread_send.start()
+    input_thread_recv.start()
+    input_thread_send.join()
+    input_thread_recv.join()
 
 
 if __name__ == '__main__':
